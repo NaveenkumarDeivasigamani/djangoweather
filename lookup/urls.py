616a3c5d5d4d3305972d8691d956d0368272dba5
naveenkumar <naveendeiva2003@gmail.com>
2024-12-23 16:28:08 +0530

    secondary commit

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,9 +1,10 @@
+# This is my views.py file
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),
-    path('about', views.about, name = 'about'),
-    
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
 
+       
 ]
